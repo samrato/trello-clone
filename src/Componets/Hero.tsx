0cd8d385e699a97fe437b88390e94627ba1f063f
Willington Juma <willingtonejumamconyango@gmail.com>
2024-11-07 15:28:38 +0300

    update

--- src/Componets/Hero.tsx
+++ src/Componets/Hero.tsx
@@ -24,11 +24,11 @@ function Hero() {
     
     console.log(list)
   return (
-    <div className="px-4 md:px-12 lg:px-20 flex flex-col md:flex-row">
+    <div className="px-4 md:px-12 lg:px-20 flex flex-col md:flex-row gap-2 md:gap-3 lg:gap-4">
 
         <div className="flex-1 bg-white text-gray-800 flex-col"> hello</div>
-        <div className="bg-blue-400 w-full"> 
-            <p><span>+</span>Add another List</p>
+        <div className="bg-blue-400 md:w-fit "> 
+            <p><span className="mx-4 font-bold">+</span>Add Another List</p>
         </div>
     
     </div>
