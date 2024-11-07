3a393ddc00c3b8cd9b9a37b3c71bd8d39c811d66
Willington Juma <willingtonejumamconyango@gmail.com>
2024-11-07 15:42:59 +0300

    update

--- src/Componets/Hero.tsx
+++ src/Componets/Hero.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Lists } from '../Constants/Type'
+import MyList from './MyList'
 
 function Hero() {
     const [list, setList] = useState<Lists> (
@@ -26,8 +27,8 @@ function Hero() {
   return (
     <div className="px-4 md:px-12 lg:px-20 flex flex-col md:flex-row gap-2 md:gap-3 lg:gap-4">
 
-        <div className="flex-1 bg-white text-gray-800 flex-col"> hello</div>
-        <div className="bg-blue-400 md:w-fit p-3 md:p-4 lg:p-6"> 
+       <MyList/>
+        <div className="bg-blue-400 md:w-fit p-3 md:p-4 lg:p-6 max-h-[60vh]"> 
             <p><span className="mx-4 font-bold">+</span>Add Another List</p>
         </div>
     
---
+++ src/Componets/MyList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyList() {
+  return (
+    <div>
+       <div className="flex-1 bg-white text-gray-800 flex-col"> hello</div>
+    </div>
+  )
+}
+
+export default MyList
