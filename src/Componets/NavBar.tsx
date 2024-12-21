50d2288435085abb3f476747e997e653acb40cbe
Willington Juma <146538882+samrato@users.noreply.github.com>
2024-12-21 21:09:50 +0300

    Update NavBar.tsx

--- src/Componets/NavBar.tsx
+++ src/Componets/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
   return (
     <header className="shadow-md p-2 flex md:flex-row flex-col gap-2 sticky">
      <div className="flex flex-row items-center justify-between w-full md:w-1/2 ">
-     <h2 className="text-xl">Welcome Board</h2>
+     <h2 className="text-xl">Welcome  Board</h2>
     <div className="bg-blue-400 p-1 rounded-sm text-white md:text-xl"> 
         <FaStarHalfStroke />
         </div>
@@ -18,7 +18,7 @@ function NavBar() {
      <div className="flex flex-row items-center justify-end w-full md:w-1/2 space-x-3 "> 
      <div className="bg-blue-400 p-1 rounded-sm hover:cursor-pointer hover:pl-5 transition-all duration-200 ease-in-out hover:bg-pink-700 ">
      <IoHomeSharp />
-         <span>Home</span> </div>
+         <span>Home </span> </div>
      <div className="bg-blue-400 p-1 rounded-sm hover:cursor-pointer hover:px-5 transition-all duration-200 ease-in-out hover:bg-pink-700"> Show Menu</div>
      </div>
     </header>
